f2ecfe7674463fc6239ff463c73e3f889e8c358c
oho_tom\tom <oho_cooh@hotmail.com>
2017-05-31 22:21:37 +0900

    ・イニシャルコミット ・pom.xml等の設定とエンティティクラス作成

---
+++ src/main/java/mrs/domain/model/MeetingRoom.java
@@ -0,0 +1,35 @@
+package mrs.domain.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class MeetingRoom implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer roomId;
+
+	private String roomName;
+
+	public Integer getRoomId() {
+		return roomId;
+	}
+
+	public void setRoomId(Integer roomId) {
+		this.roomId = roomId;
+	}
+
+	public String getRoomName() {
+		return roomName;
+	}
+
+	public void setRoomName(String roomName) {
+		this.roomName = roomName;
+	}
+
+
+}
---
+++ src/main/java/mrs/domain/model/Reservation.java
@@ -0,0 +1,73 @@
+package mrs.domain.model;
+
+import java.io.Serializable;
+import java.time.LocalTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Reservation implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer reservationId;
+
+	private LocalTime startTime;
+
+	private LocalTime endTime;
+
+	@ManyToOne
+	@JoinColumns({ @JoinColumn(name = "reserved_date"), @JoinColumn(name = "room_id")})
+	private ReservableRoom reservableRoom;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	public Integer getReservationId() {
+		return reservationId;
+	}
+
+	public void setReservationId(Integer reservationId) {
+		this.reservationId = reservationId;
+	}
+
+	public LocalTime getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(LocalTime startTime) {
+		this.startTime = startTime;
+	}
+
+	public LocalTime getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(LocalTime endTime) {
+		this.endTime = endTime;
+	}
+
+	public ReservableRoom getReservableRoom() {
+		return reservableRoom;
+	}
+
+	public void setReservableRoom(ReservableRoom reservableRoom) {
+		this.reservableRoom = reservableRoom;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+
+}
